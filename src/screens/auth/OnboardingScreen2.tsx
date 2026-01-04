3800540958c7edb7f3c2fb04cdf35023fe139edb
Ubong Jacob <ubongjacob13@gmail.com>
2026-01-04 22:59:23 +0100

    feat: add OnboardingScreen2 to display onboarding prerequisites.

--- src/screens/auth/OnboardingScreen2.tsx
+++ src/screens/auth/OnboardingScreen2.tsx
@@ -1,15 +1,60 @@
-import { View, StyleSheet, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import AppScreen from '@src/components/AppScreen';
+import AppText from '@src/components/AppText';
+import {
+  MaterialSymbolsDomainAdd,
+  MaterialSymbolsMail,
+  MaterialSymbolsPersonAdd,
+} from '@src/components/icons';
+import Size from '@src/utils/useResponsiveSize';
+import fonts from '@src/configs/fonts';
+
+const data = [
+  {
+    icon: <MaterialSymbolsDomainAdd />,
+    title: 'You reside in a SESA powered estate or gated-community.',
+  },
+  {
+    icon: <MaterialSymbolsPersonAdd />,
+    title: 'You have been onboarded by your estate manager.',
+  },
+  {
+    icon: <MaterialSymbolsMail />,
+    title:
+      'You have access to your associated email - your login details will be sent there.',
+  },
+];
 
 const OnboardingScreen2 = (): React.ReactNode => {
   return (
-    <View style={styles.container}>
-      <Text>OnboardingScreen2</Text>
-    </View>
+    <AppScreen style={styles.container}>
+      <AppText style={styles.title}>Before you get started</AppText>
+      <AppText style={styles.subTitle}>Be sure of the following</AppText>
+
+      <View>
+        {data.map((item, index) => (
+          <View key={index}>
+            <View>{item.icon}</View>
+            <AppText>{item.title}</AppText>
+          </View>
+        ))}
+      </View>
+    </AppScreen>
   );
 };
 
 const styles = StyleSheet.create({
   container: {},
+
+  subTitle: {},
+
+  title: {
+    fontSize: Size.calcAverage(24),
+    fontFamily: fonts.INTER_600,
+    textAlign: 'center',
+    marginHorizontal: 'auto',
+  },
 });
 
 export default OnboardingScreen2;

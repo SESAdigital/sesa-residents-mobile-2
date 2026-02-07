473b785d1da42e3c3cb31a561876f5156d09d69f
Ubong Jacob <ubongjacob13@gmail.com>
2026-02-07 18:47:49 +0100

    feat: Disable `react-native/no-inline-styles` and `react-hooks/exhaustive-deps` ESLint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   root: true,
   extends: '@react-native',
   rules: {
+    'react-native/no-inline-styles': 0,
+    'react-hooks/exhaustive-deps': 0,
     'no-restricted-imports': [
       'error',
       {

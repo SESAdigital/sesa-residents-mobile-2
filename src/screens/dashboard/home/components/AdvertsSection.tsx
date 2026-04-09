1a1d14f80109154b708bba1f0805e2217544511e
Ubong Jacob <ubongjacob13@gmail.com>
2026-04-09 13:58:36 +0100

    feat: add AdvertsSection component to dashboard home screen

--- src/screens/dashboard/home/components/AdvertsSection.tsx
+++ src/screens/dashboard/home/components/AdvertsSection.tsx
@@ -3,11 +3,15 @@ import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import SampleAdImage from '@src/assets/images/sample-ad.png';
 import AppImage from '@src/components/AppImage';
 import Size from '@src/utils/useResponsiveSize';
+import { openURL } from '@src/utils';
+import appConfig from '@src/utils/appConfig';
 
 const AdvertsSection = (): React.JSX.Element => {
   return (
     <View style={styles.imageContainer}>
-      <TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => openURL(`${appConfig.APP_WEBSITE_URL}/contact-use`)}
+      >
         <AppImage
           style={styles.image}
           resizeMode="contain"
